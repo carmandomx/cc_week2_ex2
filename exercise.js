380326be3a562205017d33c1992411ba86ec50ba
Daniel Bacab <Daniel.Bacab@itksquare.edu.mx>
2022-09-27 15:28:26 -0500

    Exercise done

--- exercise.js
+++ exercise.js
@@ -12,7 +12,25 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const substrGen = (str) => {
     /* Only make changes below this comment */
-    
+
+    //In this array I will save the result
+    let result = [];
+    //I need a variable to independently handle the array index of the result
+    resultIndex = 0;
+
+    //This first FOR increases the index of the letter with which the substring begins
+    for( i = 0; i < str.length; i++){
+        //This second FOR increases the index of the final letter of the substring
+        for( j = i; j < str.length; j++){
+            //The substring method is the best tool to solve this problem, because it returns the substring directly, 
+            //only passing the indices of the first letter and the last letter +1
+            result[resultIndex] = str.substring(i,j+1);
+            //Increasing the index of the result
+            resultIndex += 1;
+        }
+    }
+    return result;
+
     /* Only make changes below this comment */
 }
 
