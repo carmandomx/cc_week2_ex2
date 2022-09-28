64412f9217e087baa6df7ffe0ca7650d74540d87
Navidarito Chen <Navidarito.Chen@itksquare.edu.mx>
2022-09-28 12:43:33 -0400

    uploading week2 ex2

--- exercise.js
+++ exercise.js
@@ -12,7 +12,17 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const substrGen = (str) => {
     /* Only make changes below this comment */
-    
+    //create empty variables and empty array to store the result
+    var i, j, substrings = [];
+    //loop the string
+    for (i = 0; i < str.length; i++) {
+        for (j = i + 1; j < str.length + 1; j++) {
+            //add substring to the new array
+            substrings.push(str.slice(i, j));
+        }
+    }
+    // return the new array
+    return substrings;
     /* Only make changes below this comment */
 }
 
