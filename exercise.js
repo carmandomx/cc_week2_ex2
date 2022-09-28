bb7f704a1c26f445bb05c41ea8da7874da07064d
“Dannaé <claudia.diaz@itksquare.edu.mx>
2022-09-28 09:28:51 -0500

    Solved the substring exercise

--- exercise.js
+++ exercise.js
@@ -13,6 +13,17 @@
 const substrGen = (str) => {
     /* Only make changes below this comment */
     
+    let substr = []; // empty array to store the resulting substrings
+    
+    // a for to loop through each letter one by one
+    for (let i = 0; i < str.length; i++) {
+        // and a nested for loop to also get te following letters
+        for (let j = i+1; j < str.length+1; j++) {
+            // we slice the string in the range of i & j given by the for loops
+            substr.push(str.slice(i,j)); // and we push each slice into the substrings array
+        }
+    }
+    return substr;
     /* Only make changes below this comment */
 }
 
