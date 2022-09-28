cc9f469a033b05bdd8331d1a95aa741dd142b53e
Junior Cabrera <junior.cabrera@itksquare.edu.mx>
2022-09-28 09:56:12 -0400

    Ex2-Done

--- exercise.js
+++ exercise.js
@@ -12,6 +12,20 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const substrGen = (str) => {
     /* Only make changes below this comment */
+
+    const arr = []; //Declare an array to add the substrings and return it.
+
+    for(let i = 0; i < str.length; i++){ //Loop to go through the entire string.
+
+       arr.push(str[i]); //I add the first character of the string as a substring, since each character is a substring.
+
+        for(let j = i + 1; j < str.length; j++){ //Loop to go through string - 1 position.
+
+            arr.push(arr[arr.length - 1] + str[j]); //I search for the last character and add it together with its previous position.
+        }
+    }
+
+    return arr;
     
     /* Only make changes below this comment */
 }
