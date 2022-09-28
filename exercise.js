95621eb6877f9d90a0d4221a82ba64918cde40fc
Yumil Rueda Flores <Yumil.Flores@itksquare.edu.mx>
2022-09-28 18:52:53 +0200

    DONE YumilRueda

--- exercise.js
+++ exercise.js
@@ -12,7 +12,16 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const substrGen = (str) => {
     /* Only make changes below this comment */
-    
+    let result = []; //result
+    let len = str.length; //the lenght of the str, to use just one variable in the future
+
+    for(i=0; i<len; i++){ //2 loops to iterate with 2 elements
+        for(j=i+1; j<=len; j++){ 
+            result.push(str.slice(i, j)); //slice a part of the string, changing the size with the help od the loop
+        }
+    }
+
+    return result;
     /* Only make changes below this comment */
 }
 
