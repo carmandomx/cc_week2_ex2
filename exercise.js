d605586ea664d1bc9583933877d3d45b6d675002
Karla Chable <Karla.Chable@itksquare.edu.mx>
2022-09-27 22:48:13 -0500

    notes-EN and test

--- exercise.js
+++ exercise.js
@@ -11,15 +11,22 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const substrGen = (str) => {
   /* Only make changes below this comment */
-  let subArrStr = new Array(); //String donde almacenaremos los subarrays.
-  let subEstring = new String(); //
+  let subArrStr = new Array(); //New string for new value of substring  that we will be create.
+  let subEstring = new String(); //New array for storage our new subarrays.
+
+  //first iteration for values of "str" string.
   for (let i = 0; i < str.length; i++) {
+    //second iteration for values of "str" string.
     for (let j = i; j < str.length; j++) {
+      //We iterate until last j but we need do add +1 for includes all elements of string and assign it a var called "lastJ"
       let lastJ = j + 1;
+      //now for each new substring created re-assing the value for a substring of "str" using substring(). For using it we needs first and final position for count; i & lastJ (j + 1)
       subEstring = str.substring(i, lastJ);
+      //We push our new string in array "subArrStr".
       subArrStr.push(subEstring);
     }
   }
+  //Collect all substring and return them in "subArrStr".
   return subArrStr;
   /* Only make changes below this comment */
 };
