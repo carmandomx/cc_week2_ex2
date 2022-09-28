792e5a18eaefef8e6e748b64006d048a4871e9ce
Tomas Garcia <Tomas.Garcia@itksquare.edu.mx>
2022-09-28 10:50:06 -0500

    Task done

--- exercise.js
+++ exercise.js
@@ -12,7 +12,28 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const substrGen = (str) => {
     /* Only make changes below this comment */
-    
+
+    let substr = []; // Result string
+    let temp = []; // Temporal string to save the subtrings
+
+            // Starting point
+            for (let i = 0; i < str.length; i++) {
+
+                // Ending point
+                for (let j = i; j < str.length; j++) {
+                    
+                    // starting point to ending point
+                    for (let k = i; k <= j; k++) { // Gets the elements in the str one by one
+                        temp += str[k]; // Concats them in a temp variable
+                    }
+
+                    // The concat() method is used to merge two or more arrays
+                    substr = substr.concat(temp); // Saves the subtrings in the result variable
+                    temp = []; // cleans the temp variable to get a new string
+                }
+            }
+
+            return substr; // Returns te awnser
     /* Only make changes below this comment */
 }
 
