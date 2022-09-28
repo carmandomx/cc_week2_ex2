dd7f85b74260cb0bd29b16c851fa0de22983f180
Eduardo De Jesus <eduardo.dejesus@itksquare.edu.mx>
2022-09-28 15:35:48 -0400

    finished the exercise

--- exercise.js
+++ exercise.js
@@ -12,7 +12,14 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const substrGen = (str) => {
     /* Only make changes below this comment */
-    
+    var i, j, result = []; //added the vars that we're gonna use ofor the counter and the new array
+
+    for (i = 0; i < str.length; i++) { //first counter to go through the length of the string
+        for (j = i + 1; j < str.length + 1; j++) { //a nested counter that goes alongside the main one
+            result.push(str.slice(i, j)); //we slice the array and add it to the result while pushing the length
+        }
+    }
+    return result; //we print the result
     /* Only make changes below this comment */
 }
 
