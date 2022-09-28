d881fc603111c0586ba1caa35f017f180ec0b58a
Karla Chable <Karla.Chable@itksquare.edu.mx>
2022-09-27 22:27:22 -0500

    fc-substrGen

--- exercise.js
+++ exercise.js
@@ -8,17 +8,43 @@
 
 */
 
-
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const substrGen = (str) => {
-    /* Only make changes below this comment */
-    
-    /* Only make changes below this comment */
-}
+  /* Only make changes below this comment */
+  let subArrStr = new Array(); //String donde almacenaremos los subarrays.
+  let subEstring = new String(); //
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i; j < str.length; j++) {
+      let lastJ = j + 1;
+      subEstring = str.substring(i, lastJ);
+      subArrStr.push(subEstring);
+    }
+  }
+  return subArrStr;
+  /* Only make changes below this comment */
+};
 
-
-
-
+console.log(substrGen("abc")); //[ 'a', 'ab', 'abc', 'b', 'bc', 'c' ]
+console.log(substrGen("lagann"));
+/*       ['l',    'la',    'lag',
+        'laga', 'lagan', 'lagann',
+        'a',    'ag',    'aga',
+        'agan', 'agann', 'g',
+        'ga',   'gan',   'gann',
+        'a',    'an',    'ann',
+        'n',    'nn',    'n'
+      ]*/
+console.log(substrGen("abcabcbb"));
+/*    [ 'a',      'ab',     'abc',     'abca',
+        'abcab',  'abcabc', 'abcabcb', 'abcabcbb',
+        'b',      'bc',     'bca',     'bcab',
+        'bcabc',  'bcabcb', 'bcabcbb', 'c',
+        'ca',     'cab',    'cabc',    'cabcb',
+        'cabcbb', 'a',      'ab',      'abc',
+        'abcb',   'abcbb',  'b',       'bc',
+        'bcb',    'bcbb',   'c',       'cb',
+        'cbb',    'b',      'bb',      'b'
+      ] */
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.substrGen = substrGen;
