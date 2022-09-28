8f664312c5468026427aae5f2932f978065a5eaa
Genaro Arredondo <Genaro.Arredondo@itksquare.edu.mx>
2022-09-28 16:13:18 -0500

    Solved the 8th Exercise

--- exercise.js
+++ exercise.js
@@ -12,7 +12,13 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const substrGen = (str) => {
     /* Only make changes below this comment */
-    
+    let subStr = [];                                    //Create an array to store all possible substrings
+    for (let i = 0; i < str.length; i++) {              //Create a loop to iterate through every character from the string
+        for (let j = i + 1; j < str.length + 1; j++) {  //Create another loop to iterate through every character next to the character the first loop currently is
+            subStr.push(str.substring(i, j));           //Create a substring that starts in the current initial character to the current final character, and make a push to the array
+        }                                               //This way, the first substring would only be the first character since it ends before the next character
+    }                                                   //And then it goes one character index up, so we would have two characters in the second substring, and so on
+    return subStr;                                      //Return the array with all of the possible substrings
     /* Only make changes below this comment */
 }
 
