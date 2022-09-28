645212ba137b69496f907ce5084a9abfbed1cd53
Axcel Espinal <axcel.espinal@itksquare.edu.mx>
2022-09-28 08:25:01 -0400

    Solved

--- exercise.js
+++ exercise.js
@@ -12,12 +12,18 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const substrGen = (str) => {
     /* Only make changes below this comment */
-    
+
+    let res = []; //create an empty result array
+
+    for(i=0; i<str.length; i++){ //for loop to iterate with 1 element
+        for(j=i+1; j<=str.length; j++){ ///inner for loop to iterate with 2 elements
+            res.push(str.slice(i, j)); //slice method returns a portion of the string, so we just slice the first one and keep moving thanks to the for loop
+        }
+    }
+
+    return res; //return the result array
     /* Only make changes below this comment */
 }
-
-
-
 
 
 /** DO NOT CHANGE THE LINE BELOW **/
