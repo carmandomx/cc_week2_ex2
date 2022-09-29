a8e0da7a60b084d8b0d45e4cb57292946f9b7a0a
Roberto_Tello <Roberto.Tello@itksquare.edu.mx>
2022-09-29 05:00:14 -0500

    finished

--- exercise.js
+++ exercise.js
@@ -12,15 +12,18 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const substrGen = (str) => {
     /* Only make changes below this comment */
-    string.indexOf(substring []);
-    
-    for (let i = 0; i < arrC.length; i++) { //create a for loop inside a for loop   
-        for (let j = 0; j < arrC.length; j++) {
-
+    let arr= [];  //first we create an empty array to save our result
+    let lang = str.length;  //we use this one to check the length of the string and get a variable
+    for (let i = 0; i < lang; i++) {   //then we make 2 for cycles   
+        for (let j = i+1; j <= lang; j++) {  //j has to be i+1 cause it needs to be the next position 
+/*now we need something that cuts the strings and changes the size while using the cycle 
+this magic thing is called slice*/
+            arr.push(str.slice(i,j));//we obtain a new array
         }
     }
+    return arr;
     /* Only make changes below this comment */
-
+}
 
 
 
