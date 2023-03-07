fffa23f2e894dff073f616a4bbc9f37a47be713d
Alan Villanueva <alan.villanueva@itksquare.edu.mx>
2023-03-07 11:00:28 -0600

    solved

--- exercise.js
+++ exercise.js
@@ -11,25 +11,6 @@
 
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const substrGen = (str) => {
-    /* let one_substring = '' //Container for one single substring
-    let str_arr=str.split("") //str made and array. 'hola'---->['h','o','l','a']
-    let subarray=[] // container of sub array after slicing str_array
-    const n=str.length // string length
-    let sub_strings_container=[] //container of all substrings
-    for (let i=1; i<n+1; i++){ //size of the substrings to take
-        for (let j=0;j<(n-i+1);j++){ //starting point of the substring to take from string
-            one_substring='' //reset substring to take
-            subarray = str_arr.slice(j,j+i) //we take a slice of the str_arr containing all the substring characters
-            for ( let l=0;l<subarray.length;l++){ //we stick together the substring from the array
-                one_substring+=subarray[l]
-            }
-            sub_strings_container.push(one_substring) //add the substring created to the array containing all substrings
-
-
-            
-        }
-
-    }*/
     let one_substring = '' //Container for one single substring
     let str_arr=str.split("") //str made and array. 'hola'---->['h','o','l','a']
     let subarray=[] // container of sub array after slicing str_array
@@ -49,6 +30,5 @@ const substrGen = (str) => {
 
     return sub_strings_container
 }
-console.log(substrGen('abcabcbb'))
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.substrGen = substrGen;
