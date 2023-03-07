7b02b6d55413eaff15cd80f410c2539e5f3e1148
Andrei Santos <Andrei.Santos@itksquare.edu.mx>
2023-03-07 10:12:46 -0600

    cc_week2_ex2 AndreiSantos

--- exercise.js
+++ exercise.js
@@ -12,7 +12,22 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const substrGen = (str) => {
     /* Only make changes below this comment */
-    
+    let newArr = str.split('');
+    let answer = [];
+    newArr.forEach((element, index) => {
+        let tempArr = [];
+        for (let count = index; count < newArr.length; count++) {
+          tempArr.push(str[count]);
+        }
+        tempArr.forEach((element, index2) => {
+          let tempArr2 = []
+          for (let count = 0; count <= index2; count++) {
+            tempArr2.push(tempArr[count]);
+          }
+          answer.push(tempArr2.join(''));
+        });
+    });
+    return answer
     /* Only make changes below this comment */
 }
 
