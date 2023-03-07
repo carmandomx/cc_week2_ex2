508798de768188054fe67339dc41b4b5dd2d0302
Rita Torres <taraumara96@gmail.com>
2023-03-07 13:08:53 -0600

    SubStrings

--- exercise.js
+++ exercise.js
@@ -8,17 +8,21 @@
 
 */
 
-
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const substrGen = (str) => {
-    /* Only make changes below this comment */
-    
-    /* Only make changes below this comment */
-}
+  /* Only make changes below this comment */
+  
+  let newArr=str.split('');
+  let answer = [];
+  for (let i = 0; i < newArr.length; i++) {
+    for (let j = i + 1; j <= newArr.length; j++) {
+      answer.push(str.slice(i, j));
+    }
+  }
+  return answer;
 
-
-
-
+  /* Only make changes below this comment */
+};
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.substrGen = substrGen;
