d3c035f311818a57930bbe2026b72669738d3571
BraulioEstrada-Ksquare <Braulio.Estrada@itksquare.edu.mx>
2023-03-07 13:21:34 -0600

    ejercicio resuelto

--- exercise.js
+++ exercise.js
@@ -12,7 +12,15 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const substrGen = (str) => {
     /* Only make changes below this comment */
-    
+    let answ = []; // We use and empty array for the answer
+    for(i=0;i<str.length;i++){ //for loop with variable i, this loop is for changing the starting point of the for loop "j"
+        let stranw = [];
+        for(j=0;j<str.length-i;j++){ //for loop with variable j wich will iterate through the string, we rest i to the string lenght because the start point moves one place every for loop "i"
+            stranw += str[j+i];//using i+j to change the interation start point
+            answ.push(stranw);//we save the answer in the last element of the array
+        }
+    }
+    return answ;
     /* Only make changes below this comment */
 }
 
