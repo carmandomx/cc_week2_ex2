dd9cb5fe4c3b6ca8d5b12444aaee4626ecd21dba
AlexanderSuarez-Ksquare <125915698+AlexanderSuarez-Ksquare@users.noreply.github.com>
2023-03-10 13:04:05 -0600

    Add comments

--- exercise.js
+++ exercise.js
@@ -11,13 +11,14 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const substrGen = (str) => {
   /* Only make changes below this comment */
-  const substring = [];
-  for (let i = 0; i < str.length; i++) {
+  const substring = [];  //Se crea un array donde iran nuestros substrings
+  for (let i = 0; i < str.length; i++) { //Tengo dos iteradores, i avanzará cada vez que j recorra el array completo
     for (let j = i + 1; j <= str.length; j++) {
-      substring.push(str.slice(i, j));
+      substring.push(str.slice(i, j)); //empujo al array que cree al principio un slice que ira creciendo conforme j avanza.
+      //La primera iteración sera str.slice(0,1) que es igual a ["a"], por ejemplo, la segunda iteracion será str.slice(0,2) que es igual a ["ab"], y así hasta llegar al final del array.
     }
   }
-  return substring;
+  return substring; //retorno del alrray que creamos lleno con los valores de las iteraciones.
 };
 /* Only make changes below this comment */
 
